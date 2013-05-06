cbb36167b7955a807cef17b6ec39582dd96474ac
Éric Brison <eric.brison@anakeen.com>
2013-05-06 09:25:02 +0200

    add pagination

--- Scripts/zoo_addchild.php
+++ Scripts/zoo_addchild.php
@@ -9,8 +9,8 @@
  * @package freedom-zoo
  * 
  *
- * @global docid Http var : document identificator
- * @global n Http var : number of new childs
+ * @global string $docid Http var : document identificator
+ * @global string $n Http var : number of new childs
  /**
  */
 
@@ -18,45 +18,46 @@
 include_once("FDL/Class.Doc.php");
 
 $usage="usage  --docid=<doc identificator> --n=<number of child>";
-
+/**
+ * @var Action $action
+ */
 $dbaccess=$action->GetParam("FREEDOM_DB");
 if ($dbaccess == "") {
   print "Freedom Database not found : param FREEDOM_DB";
   exit;
 }
+$usage=new ApiUsage();
+$usage->setText("create somes childs");
+$docid=$usage->addRequiredParameter("docid","animal parent document identifier");
+$n=intval($usage->addRequiredParameter("n","count of child needed")); // number of childs
+$usage->verify();
 
-$docid = $action->getArgument("docid",0); // special docid
-$n = intval($action->getArgument("n")); // number of childs
-
-if (! $n)   $action->exitError("n needed :\n $usage");  
-if ($n<0) $action->exitError("n must be greater than 0 :\n $usage");  
+if ($n<0) $action->exitError(sprintf("n must be greater than 0 :%s", $usage->getUsage()));
 
 $doc=new_doc($dbaccess,$docid);
 if ($doc->isAlive()) {
   $animalid=getFamIdFromName($dbaccess,'ZOO_ANIMAL');
   if ($doc->fromid != $animalid) {
-    $fdoc=$doc->getFamDoc();
+    $fdoc=$doc->getFamilyDocument();
     $action->exitError(sprintf("%s [%d] document is not an animal (it is a %s)",
 			       $doc->getTitle(),$doc->id,$fdoc->getTitle()));
   }
   
-  $childs=$doc->getTValue("an_enfant");
+  $childs=$doc->getMultipleRawValues("an_enfant");
   $nc=count($childs);
-
+$err='';
   for ($i=0;$i<$n;$i++) {
     $anchild=createDoc($dbaccess,"ZOO_ANIMAL");
     if (! $anchild) $action->exitError("cannot create ANIMAL");
     $anchild->setValue("an_nom",sprintf("%s Junior %d",
-					$doc->getValue("an_nom"),($nc+$i+1)));
-    $err=$anchild->setValue("an_espece",$doc->getValue("an_espece"));
+					$doc->getRawValue("an_nom"),($nc+$i+1)));
+    $err=$anchild->setValue("an_espece",$doc->getRawValue("an_espece"));
     $err.=$anchild->setValue("an_naissance",$doc->getDate());
     $err.=$anchild->setValue("an_entree",$doc->getDate());    
-    if (! $err) $err=$anchild->Add();      
+    if (! $err) $err=$anchild->store();
     if ($err != "") $action->exitError($err);
-    $anchild->refresh();
-    $anchild->postModify();
     printf("%s [%d] created\n",$anchild->getTitle(),$anchild->id);
-    $childs[]=$anchild->getProperty('initid');		    
+    $childs[]=$anchild->getPropertyValue('initid');
   }
 
   if ($err != "") $action->exitError($err);
